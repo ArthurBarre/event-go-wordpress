1dbcd6d31396322d08bdf7062019ecdbdb59e68f
Arthur Barre <arthurbarre.js@gmail.com>
2021-05-20 13:00:43 +0200

    add event page

---
+++ wp-content/themes/twentytwentyone/single-event.php
@@ -0,0 +1,28 @@
+<?php
+
+$event_args = array(
+    'post_type'=>'event',
+    "posts_per_page"=>3,
+    'orderby'=>'date',
+    'order'=>'desc'
+);
+
+$last_events = new WP_Query( $event_args );
+
+ob_start();
+
+?>
+
+<div>
+<?php
+    if($last_events->have_posts()){
+        while ($last_events->have_posts()) {
+            $last_events->the_post();
+            ?>
+            <div>
+                <div> <?php the_title(); ?> </div>
+            </div>
+        }
+    }
+
+</div>
